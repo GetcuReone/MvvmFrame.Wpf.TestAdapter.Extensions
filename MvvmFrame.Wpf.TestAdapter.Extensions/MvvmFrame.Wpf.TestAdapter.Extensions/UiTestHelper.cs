dcb77e117aa9baa75c5caedae41441026c6cd483
Juel <GetcuReone@hotmail.com>
2020-12-19 17:41:25 +0300

    [optimization] Project transfer to GitHub Fixes

---
+++ MvvmFrame.Wpf.TestAdapter.Extensions/MvvmFrame.Wpf.TestAdapter.Extensions/UiTestHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Controls;
+
+namespace GetcuReone.MvvmFrame.Wpf.TestAdapter
+{
+    /// <summary>
+    /// Helper for UI test.
+    /// </summary>
+    public static class UiTestHelper
+    {
+        /// <summary>
+        /// Check type and get data context.
+        /// </summary>
+        /// <typeparam name="TDataContext"></typeparam>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        public static TDataContext CheckTypeAndDataContext<TDataContext>(this Page page)
+        {
+            if (page.DataContext is TDataContext dataContext)
+                return dataContext;
+
+            Assert.IsNotNull(page.DataContext, "DataContext is null.");
+            Assert.Fail($"DataContext has type {page.DataContext.GetType().Name}. Expected<{typeof(TDataContext).Name}>.");
+            return default;
+        }
+    }
+}
